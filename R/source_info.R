451397cdc866199aad87df979a7a4e1e13549fb8
Aaron Chafetz <achafetz@usaid.gov>
2024-02-26 16:06:22 -0500

    fix: increase base year of default file, resolves #34

--- R/source_info.R
+++ R/source_info.R
@@ -169,7 +169,7 @@ extract_metadata <- function(path, type){
     stop("File/folder do not exist or path is not correct.")
 
   if(file.info(path)$isdir && missing(type))
-    type <- "OU_IM_FY21"
+    type <- "OU_IM_FY22"
 
   if(file.info(path)$isdir)
     path <- glamr::return_latest(path, type)
